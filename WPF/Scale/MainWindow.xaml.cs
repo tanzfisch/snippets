9ec054085ad402a3ea01a05342a740ee3d1bd4ad
tanzfisch <tanzfisch@gmail.com>
2016-07-22 14:19:02 +0200

    full window scaling in wpf

---
+++ WPF/Scale/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows;
+
+namespace COBRA
+{
+
+    /*! main window of Logger
+     */
+    public partial class MainWindow : Window
+    {
+
+        /*! initialize member variables
+        */
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        #region ScaleValue Depdency Property
+        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
+
+        private static object OnCoerceScaleValue(DependencyObject o, object value)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+            {
+                return mainWindow.OnCoerceScaleValue((double)value);
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.OnScaleValueChanged((double)e.OldValue, (double)e.NewValue);
+            }
+        }
+
+        protected virtual double OnCoerceScaleValue(double value)
+        {
+            if (double.IsNaN(value))
+            {
+                return 1.0f;
+            }
+
+            value = Math.Max(0.1, value);
+            return value;
+        }
+
+        protected virtual void OnScaleValueChanged(double oldValue, double newValue)
+        {
+
+        }
+
+        public double ScaleValue
+        {
+            get
+            {
+                return (double)GetValue(ScaleValueProperty);
+            }
+            set
+            {
+                SetValue(ScaleValueProperty, value);
+            }
+        }        
+
+        private void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            CalculateScale();
+        }
+
+        private void CalculateScale()
+        {
+            double yScale = ActualHeight / 768;
+            double xScale = ActualWidth / 1024;
+            double value = Math.Min(xScale, yScale);
+            ScaleValue = (double)OnCoerceScaleValue(TheMainWindow, value);
+        }
+        #endregion
+
+    }
+}
+
+
